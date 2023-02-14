1d8f6a2e1e0a9568857e8efad6a05b61179c756d
AngelMorquecho <108309811+AngelMorquecho@users.noreply.github.com>
2023-02-13 21:37:11 -0600

    Update crearUsuario.php

--- crearUsuario.php
+++ crearUsuario.php
@@ -1,9 +1,14 @@
 <?php 
-header("Content-Type: application/json");
-$host="192.168.100.13";
-$user="ANGEL";
-$password="Root1Angel";
-$dataBase="OSFESA";
+
+header("Content-Type:application/json");
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$host="162.241.62.132";
+$user="grupoosf_admin_pruebas";
+$password="rQbp2RZAPK7KSBs";
+$dataBase="grupoosf_db_pruebas";
 $obj=json_decode(file_get_contents('php://input'), true);
 $response = array();
 $response['status'] = false;
@@ -15,14 +20,14 @@ $usuarioId = $obj["userId"];
 $password = $obj["password"];
 $user_name = $obj["nombre"];
 $level = $obj["level"];
-$estatus = $obj["estatus"];
+//$estatus = $obj["estatus"];
 
 
 if(!$connection){
     die("Connection failed: " . mysqli_connect_error());
 
 }
-$sql = "INSERT INTO user_admin VALUES ('$usuarioId','$password','$user_name','$level','$estatus')";
+$sql = "INSERT INTO USER_ADMIN VALUES ('$usuarioId','$password','$user_name','$level','ACTIVO')";
 
 
 
@@ -34,3 +39,4 @@ if (mysqli_query($connection, $sql)) {
 
 
 ?>
+
