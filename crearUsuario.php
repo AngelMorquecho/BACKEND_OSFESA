ca034a2c46a9d5327996e4d60413ec6b2e743d1f
AngelMorquecho <108309811+AngelMorquecho@users.noreply.github.com>
2023-02-18 16:48:18 -0600

    Update crearUsuario.php

--- crearUsuario.php
+++ crearUsuario.php
@@ -1,20 +1,17 @@
 <?php 
-
+include 'index.php';
 header("Content-Type:application/json");
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
-$host="162.241.62.132";
-$user="grupoosf_admin_pruebas";
-$password="rQbp2RZAPK7KSBs";
-$dataBase="grupoosf_db_pruebas";
+
+
 $obj=json_decode(file_get_contents('php://input'), true);
 $response = array();
 $response['status'] = false;
 
 
-$connection=new mysqli($host,$user,$password,$dataBase);
 
 $usuarioId = $obj["userId"];
 $password = $obj["password"];
@@ -39,4 +36,3 @@ if (mysqli_query($connection, $sql)) {
 
 
 ?>
-
